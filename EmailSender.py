fd1e24d57ea8481832b10120aadd9f60dacff8f7
Sam-arth <77962701+Sam-arth@users.noreply.github.com>
2021-05-19 12:45:28 +0530

    Add files via upload

---
+++ EmailSender.py
@@ -0,0 +1,25 @@
+#import  
+import smtplib 
+  
+# SMTP session creator
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("From", "Password") 
+
+#Subject 
+subject = "Subject Here"  
+
+# message to be sent 
+message = "Message here"
+  
+# sending the mail 
+s.sendmail("From", "To" , message) 
+  
+# terminating the session 
+s.quit() 
+
+print("sent")
